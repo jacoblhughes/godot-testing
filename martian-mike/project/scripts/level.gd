fa70ba0cdc6bb4949f0dfecf3b654fd8ad7c4099
Jacob Hughes <hughes.jacobl@gmail.com>
2023-11-22 18:59:38 -0800

    started

---
+++ martian-mike/project/scripts/level.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var start_position = $StartPosition
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+	pass
+
+
+func _on_deathzone_body_entered(body):
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
+	pass # Replace with function body.
