778189ceaba27e4b93532cd8376e86de689ed130
Jacob Hughes <hughes.jacobl@gmail.com>
2023-11-24 12:37:15 -0800

    wow

--- circle-bounce-test/ball.gd
+++ circle-bounce-test/ball.gd
@@ -1,29 +1,37 @@
 extends RigidBody2D
 
 var color_rect
-var trail = []
-var my_timer : Timer
+@onready var game = get_parent().get_node("Game")
+var timer_node : Timer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	color_rect = $ColorRect
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
 	# Add the current position and color to the trail
-	trail.append({"pos": position, "color": Color.AQUA})
-	await 
-	print(trail)
+
+
+func _on_level_timer_timeout():
+
+	game.trail.append({"pos": position, "color": Color.AQUA})
+	queue_redraw()
+
 	# Limit the trail length to a certain number of points
 
 # Called every frame for drawing.
 func _draw():
-	# Draw the trail
-	for point in trail:
-		draw_rect(Rect2(point["pos"] - Vector2(color_rect.size.x / 2, color_rect.size.y / 2), color_rect.size), point["color"])
+	print(.trail)
+	for point in game.trail:
+		print(point)
+		draw_rect(Rect2(point["pos"], color_rect.size), point["color"])
 
 
 func _on_body_entered(body):
-	print('here')
+
 	linear_velocity *= 1.1
 	
 	pass # Replace with function body.
