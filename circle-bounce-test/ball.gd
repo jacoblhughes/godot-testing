dcb4a8c3e36115a621456bb4fb6eb28a5ea5918d
Jacob Hughes <hughes.jacobl@gmail.com>
2023-11-24 19:29:50 -0800

    doublequare

--- circle-bounce-test/ball.gd
+++ circle-bounce-test/ball.gd
@@ -1,26 +1,48 @@
 extends RigidBody2D
 
-var color_rect
-@onready var game = get_parent().get_node("Game")
 
-
-
+@onready var game = get_parent()
+@export var color_rect : ColorRect
+var timer_node
+signal wall_collision(collision_info)
+var random_color
+var music_notes 
+var note = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	timer_node = get_parent().get_node("Timer")
+	timer_node.timeout.connect(_on_timer_timeout)
 	color_rect = $ColorRect
 	game.set_color_rect_size(color_rect.size)
-
-
+	music_notes = get_parent().get_node("Music").get_children()
+	random_color = Color(randf(), randf(), randf(), 1.0)
+	game.add_position_info(position,random_color)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	# Add the current position and color to the trail
 
-
+func _on_timer_timeout():
+	print(position,random_color)
+	game.add_position_info(position,random_color)
+	pass
 
 
 	# Limit the trail length to a certain number of points
 
 # Called every frame for drawing.
 
+
+
+func _on_body_entered(body):
+	random_color = Color(randf(), randf(), randf(), 1.0)
+	color_rect.color = random_color
+#	music_notes[note].play()
+#	print(body)
+	body.get_node("ColorRect").color = random_color
+#	note+=1
+#	if (note == 4):
+#		note = 0
+	linear_velocity *= 1.05
+	
+	pass # Replace with function body.
--- circle-bounce-test/game.gd
+++ circle-bounce-test/game.gd
@@ -3,61 +3,32 @@ extends Node2D
 @export var trail = []
 var my_size : Vector2
 var timer_node : Timer
-var ball
-var color_rect
+var balls
+#var color_rect
 var random_color
 var music_notes
-var note
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ball = get_parent().get_node("Ball")
-	color_rect = ball.get_node("ColorRect")
-	timer_node = Timer.new()
-	timer_node.name = "Level Timer"
-	timer_node.wait_time = .25
-	timer_node.timeout.connect(_on_level_timer_timeout)
-	add_child(timer_node)
-	timer_node.start()
-	random_color = Color(randf(), randf(), randf(), 1.0)
-	color_rect.color = random_color
-	trail.append({"pos": ball.position, "color": color_rect.color})
-	music_notes = get_parent().get_node("Music").get_children()
-	note = 0
+
 	pass # Replace with function body.
 
 func set_color_rect_size(node_size):
-	my_size = node_size
+	my_size = Vector2(16,16)
 
 func _draw():
 	for point in trail:
 		draw_rect(Rect2(point["pos"] - Vector2(my_size.x/2,my_size.y/2),my_size), point["color"])
 
-
+func add_position_info(ball_position,ball_color):
+	trail.append({"pos": ball_position, "color": ball_color})
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(trail.size() >1):
 		queue_redraw()
 	pass
-	
-func _on_level_timer_timeout():
 
-
-	trail.append({"pos": ball.position, "color": random_color})
-
-
-
-func _on_ball_body_entered(body):
-	random_color = Color(randf(), randf(), randf(), 1.0)
-	color_rect.color = random_color
-	body.get_node("ColorRect").color = random_color
-	ball.linear_velocity *= 1.05
-	print(note)
-	music_notes[note].play()
-	note+=1
-	if (note == 4):
-		note = 0
-	pass # Replace with function body.
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_accept"):
