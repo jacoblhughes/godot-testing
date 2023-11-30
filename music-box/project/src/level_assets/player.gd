ce8d4ce15809cceb5d21b4009fa88fdefa9547ad
Jacob Hughes <hughes.jacobl@gmail.com>
2023-11-29 16:24:13 -0800

    more

--- music-box/project/src/level_assets/player.gd
+++ music-box/project/src/level_assets/player.gd
@@ -3,6 +3,7 @@ class_name Player
 
 const SPEED = 200.00
 const JUMP_VELOCITY = 200.00
+var active = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,14 +14,12 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	var direction = 0
+	if active == true:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			jump(JUMP_VELOCITY)
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump(JUMP_VELOCITY)
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
+		direction = Input.get_axis("move_left", "move_right")
 	if direction !=0:
 		animated_sprite.flip_h = (direction < 0)
 
--- music-box/project/src/levels/level.gd
+++ music-box/project/src/levels/level.gd
@@ -2,18 +2,21 @@ extends Node2D
 
 @onready var player = null
 @onready var start = $Start
+@onready var exit = $Exit
+@onready var death_zone = $DeathZone
+@export var next_level : PackedScene = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
-
+	
 	if player != null:
 		player.global_position = start.get_spawn_position()
 	var traps = get_tree().get_nodes_in_group("traps")
 	for trap in traps:
 		trap.touched_player.connect(_on_trap_touched_player)
 	#add the quit and stuff from the other one
-
-
+	exit.body_entered.connect(_on_exit_body_entered)
+	death_zone.body_entered.connect(_on_death_zone_body_entered)
 	pass # Replace with function body.
 
 
@@ -38,3 +41,11 @@ func _on_trap_touched_player():
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start.get_spawn_position()
+
+func _on_exit_body_entered(body):
+	if body is Player:
+		exit.animate()
+		player.active = false
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_packed(next_level)
+	
