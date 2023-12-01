b36f43a2a07c05bc70f93479889320bf8fd97e19
Jacob <hughes.jacobl@gmail.com>
2023-11-30 21:46:39 -0800

    word salad

--- music-box/project/src/level_assets/player.gd
+++ music-box/project/src/level_assets/player.gd
@@ -7,20 +7,22 @@ var active = true
 var under_water = false
 var falling = false
 var idle_time = 0
+var double_jump_unlocked = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var ray_cast = $RayCast2D
+
 func _physics_process(delta):
-	print(ray_cast.get_collider())
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	var direction = 0
 	if active == true:
 		if under_water and Input.is_action_just_pressed("jump"):
 			jump(JUMP_VELOCITY/2)
-		if !under_water and Input.is_action_just_pressed("jump") and is_on_floor():
+		if !under_water and Input.is_action_just_pressed("jump") and Input.is_action_pressed("move_down"):
+			global_position.y +=1
+		elif !under_water and Input.is_action_just_pressed("jump") and is_on_floor():
 			jump(JUMP_VELOCITY)
 
 		direction = Input.get_axis("move_left", "move_right")
