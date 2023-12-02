9412eb3d10159094aa001046007e827c3edff059
Jacob Hughes <hughes.jacobl@gmail.com>
2023-12-01 21:27:16 -0800

    level+1

--- music-box/project/src/level_assets/bg.gd
+++ music-box/project/src/level_assets/bg.gd
@@ -1,20 +1,21 @@
 extends ParallaxBackground
 
-@export var bg_texture: CompressedTexture2D = preload("res://textures/blue.png")
+@export var far_texture: CompressedTexture2D = preload("res://textures/level_1/far_1.png")
+@export var close_texture: CompressedTexture2D = preload("res://textures/level_1/middle_1.png")
+@export var scroll_speed = 10
 
-@export var scroll_speed = 15
-
-@onready var sprite = $ParallaxLayer/Sprite2D
-
+@onready var far_sprite = $ParallaxLayer/Sprite2D
+@onready var close_sprite = $ParallaxLayer2/Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite.texture = bg_texture
+	far_sprite.texture = far_texture
+	close_sprite.texture = close_texture
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	sprite.region_rect.position += delta * Vector2(scroll_speed,scroll_speed)
-	if sprite.region_rect.position >= Vector2(64,64):
-		sprite.region_rect.position = Vector2.ZERO
+func _physics_process(delta):
+	far_sprite.region_rect.position += delta * Vector2(scroll_speed,0)
+	if far_sprite.region_rect.position >= Vector2(1024,0):
+		far_sprite.region_rect.position = Vector2.ZERO
 	pass
