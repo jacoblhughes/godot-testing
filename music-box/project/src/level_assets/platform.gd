0053a0716ea7e2b67fd15117708855d36a786e6b
Jacob <hughes.jacobl@gmail.com>
2023-12-03 15:36:38 -0800

    mmore

--- music-box/project/src/level_assets/platform.gd
+++ music-box/project/src/level_assets/platform.gd
@@ -3,7 +3,8 @@ extends Node2D
 @export var platform_choice = 1
 
 @onready var animation_player = $AnimationPlayer
-
+@onready var animatable_body = $AnimatableBody2D
+@export var platform_scale : Vector2 = Vector2(1,1)
 # Get the current position of the animation in seconds
 
 
@@ -11,7 +12,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+	scale = platform_scale
 #	animation_player.get_animation("choice").seek(2)
 #	var current_position = animation_player.get_position()	
 	animation_player.set_current_animation("choice")
@@ -25,6 +26,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
 	pass
 
 func testing():
