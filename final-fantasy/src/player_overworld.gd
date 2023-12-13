ac5b30e8294343b052e4b7e6b4723e2a47cd2d40
Jacob <hughes.jacobl@gmail.com>
2023-12-13 15:50:10 -0800

    more

--- final-fantasy/src/player_overworld.gd
+++ final-fantasy/src/player_overworld.gd
@@ -1,18 +1,33 @@
 extends CharacterBody2D
 
 
-const SPEED = 25.0
+const SPEED = 35.0
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite2D
 
 func _physics_process(delta):
-
 	var direction_x = Input.get_axis("move_left", "move_right")
-	if direction_x:
-		velocity.x = direction_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 	var direction_y = Input.get_axis("move_up", "move_down")
-	if direction_y:
+
+	# Reset velocity
+	velocity = Vector2.ZERO
+
+	# Prioritize horizontal movement
+	if direction_x != 0:
+		velocity.x = direction_x * SPEED
+		if direction_x < 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
+		animation_player.play("move_side")
+	# If no horizontal movement, check for vertical movement
+	elif direction_y != 0:
 		velocity.y = direction_y * SPEED
+		if direction_y > 0:
+			animation_player.play("move_down")
+		else:
+			animation_player.play("move_up")
 	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
+		animation_player.stop()
+
 	move_and_slide()
