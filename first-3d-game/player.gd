69fac27e2ff6d2c3380c0d31d6e0769382f46cfb
Jacob <hughes.jacobl@gmail.com>
2023-12-19 12:39:16 -0800

    more

--- first-3d-game/player.gd
+++ first-3d-game/player.gd
@@ -23,7 +23,9 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
-
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -58,7 +60,7 @@ func _physics_process(delta):
 	velocity = target_velocity
 	
 	move_and_slide()
-
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 func _on_mob_detector_body_entered(body):
 	die()
