bd30faa9b085919683a803069569d3885f3a701e
Jacob <hughes.jacobl@gmail.com>
2024-01-20 20:06:37 -0800

    more

--- instrument-battle/player.gd
+++ instrument-battle/player.gd
@@ -3,38 +3,44 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@onready var instrument_sound : GPUParticles2D = $Instrument/GPUParticles2D
-
+@onready var attack_animation : AnimationPlayer = $AnimationPlayer
+@onready var blast_area : Area2D = $Instrument/Attack/Blast
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
+	attack_animation.animation_finished.connect(_on_attack_finished)
+	blast_area.hit_ghost_enemy.connect(_hit_enemy)
 	pass
 
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("play_instrument"):
-		instrument_sound.emitting = true
+		attack_animation.play("blast")
+		pass
+	var direction_y = Input.get_axis("move_up", "move_down")
+	if direction_y:
+		velocity.y = direction_y * SPEED
 
 	else:
-		instrument_sound.emitting = false
-	
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
-		instrument_sound.process_material.initial_velocity_max += SPEED
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		instrument_sound.process_material.initial_velocity_max += SPEED
+
 	move_and_slide()
+
+func _on_attack_finished(anim_name: String):
+	if anim_name == "blast":
+		attack_animation.play("RESET")
+	pass
+
+func _hit_enemy():
+	print("enemy hit")
