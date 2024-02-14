58716df447677e5ecfe634871d9d3163490cffed
Jacob Hughes <hughes.jacobl@gmail.com>
2024-02-13 18:52:08 -0800

    more

---
+++ circle-bounce-test/generic/ball_basic.gd
@@ -0,0 +1,38 @@
+extends RigidBody2D
+
+
+@onready var game = get_parent()
+var sprite : Sprite2D
+
+signal wall_collision(current_position,current_velocity)
+var random_color
+var music_notes 
+var note = 0
+var texture
+var this_scale
+
+func _ready():
+	if has_node("Sprite2D"):
+		sprite = $Sprite2D
+		random_color = Color(randf(), randf(), randf(), 1.0)
+		texture = sprite.texture
+		sprite.modulate = random_color
+		this_scale = sprite.scale
+
+func _process(_delta):
+	pass
+
+func _on_timer_timeout():
+
+	game.add_position_info(position,random_color,texture,this_scale)
+	pass
+
+func _on_body_entered(body):
+	random_color = Color(randf(), randf(), randf(), 1.0)
+	body.get_node("ColorRect").color = random_color
+	if body is Wall:
+		body.get_node("AudioStreamPlayer").play()
+		sprite.modulate = random_color
+#		linear_velocity *= 1.05
+#		wall_collision.emit(position,linear_velocity)
+	pass
