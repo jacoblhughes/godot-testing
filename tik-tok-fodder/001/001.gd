98880c6666aae11bac51b4f2c95b5dfd499230d9
Jacob <hughes.jacobl@gmail.com>
2024-02-16 13:32:36 -0800

    more

--- tik-tok-fodder/001/001.gd
+++ tik-tok-fodder/001/001.gd
@@ -5,25 +5,44 @@ var wall_elements
 var ball_elements
 var grav_elements
 var ball_instance_scene = preload("res://generic/ball.tscn")
+var trail : Array = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	wall_elements = get_tree().get_nodes_in_group("wall")
 	load_sounds_and_assign_to_walls()
+	
 	ball_elements = get_tree().get_nodes_in_group("ball")
+	
 	for element in ball_elements:
 		element.wall_collision_and_split.connect(_on_wall_collision_and_split)
+		element.add_position_to_trail.connect(_on_add_position_to_trail)
 		pass
+		
 	grav_elements = get_tree().get_nodes_in_group("gravity")
-	pass
 
 
-func _on_wall_collision_and_split(pos,vel,split_value):
 
+		
+		
+	for i in len(grav_elements):
+		print(i)
+		var collision_shape = grav_elements[i].get_node("CollisionShape2D")
+		print(collision_shape)
+		if i==0:
+			grav_elements[i].set_gravity_point_center(Vector2(collision_shape.shape.get_rect().size.x/2,collision_shape.shape.get_rect().size.y/2))
+			print(grav_elements[i].get_gravity_point_center())
+		elif i==1:
+			grav_elements[i].set_gravity_point_center(Vector2(collision_shape.shape.get_rect().size.x/2,collision_shape.shape.get_rect().size.y/4))
+			print(grav_elements[i].get_gravity_point_center())
+			
+func _on_wall_collision_and_split(pos,vel,split_value,splits):
 	var ball_instance = ball_instance_scene.instantiate()
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(splits).timeout
 	ball_instance.position = pos
 	ball_instance.linear_velocity = vel
 	ball_instance.wall_collision_and_split.connect(_on_wall_collision_and_split)
+	ball_instance.add_position_to_trail.connect(_on_add_position_to_trail)
+	ball_instance.hit_wall_speed_up = true
 	add_child.call_deferred(ball_instance,true)
 
 func load_sounds_and_assign_to_walls():
@@ -36,3 +55,15 @@ func load_sounds_and_assign_to_walls():
 			wall_elements[i].add_child(audio_player,true)
 		else:
 			print("Failed to load sound:", sound_files[sound_index])
+
+func _draw():
+	for point in trail:
+		draw_texture_rect(point["texture"],Rect2(point["pos"]-(point["texture"].get_size()*point["this_scale"]/2),point["texture"].get_size()*point["this_scale"]),false,point["color"])
+
+func _process(_delta):
+	if(trail.size() >=1):
+		queue_redraw()
+
+func _on_add_position_to_trail(ball_position,ball_color,this_texture,this_scale):
+	trail.append({"pos": ball_position, "color": ball_color,"texture":this_texture,"this_scale":this_scale})
+
--- tik-tok-fodder/generic/ball.gd
+++ tik-tok-fodder/generic/ball.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 @onready var game = get_parent()
 #var basic_ball_scene = preload("res://generic/ball.tscn")
 signal wall_collision_and_split(current_position,current_velocity,split_number)
+signal add_position_to_trail(ball_position,ball_color,this_texture,this_scale)
 var random_color
 var music_notes 
 var note = 0
@@ -16,7 +17,7 @@ var texture
 @export var hit_wall_grow_ball = false
 @export var grow_ball_value = 1.05
 @export var hit_wall_speed_up = false
-@export var speed_up_value = 1.05
+@export var speed_up_value = 1.1
 @export var hit_wall_split = false
 @export var split_value = 2
 var splits = 0
@@ -32,7 +33,7 @@ func _ready():
 
 	if start_with_linear_velocity:
 		start_linear_velocity()
-		
+	
 		
 func _process(_delta):
 	pass
@@ -41,7 +42,6 @@ func start_linear_velocity():
 	linear_velocity = starting_linear_velocity_direction * starting_linear_velocity_value
 
 func _on_body_entered(body):
-	print(splits)
 	if body is Wall:
 
 		if hit_wall_apply_color_to_wall:
@@ -57,8 +57,9 @@ func _on_body_entered(body):
 		if hit_wall_split:
 			if infinite_splits or splits<allowed_splits:
 				print("split")
-				wall_collision_and_split.emit(global_position,linear_velocity,split_value)
 				splits+=1
+				wall_collision_and_split.emit(global_position,linear_velocity,split_value,splits)
+
 		pass
 		if hit_wall_play_sound:
 			body.get_parent().get_node("AudioStreamPlayer").play()
@@ -66,3 +67,8 @@ func _on_body_entered(body):
 		print('okay but')
 	pass
 
+
+
+func _on_timer_timeout():
+	add_position_to_trail.emit(global_position,%Sprite2D.modulate,%Sprite2D.texture,%Sprite2D.scale)
+	pass # Replace with function body.
