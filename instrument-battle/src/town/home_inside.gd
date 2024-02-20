e8c9c3347f2def9674f039cbef49f1ca82c0878f
Jacob Hughes <hughes.jacobl@gmail.com>
2024-02-19 20:19:26 -0800

    more

--- instrument-battle/src/town/home_inside.gd
+++ instrument-battle/src/town/home_inside.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	%Player.get_node("AnimationPlayer").play("collision_village")
 	NavigationManager.on_level_spawn()
 
 	pass # Replace with function body.
