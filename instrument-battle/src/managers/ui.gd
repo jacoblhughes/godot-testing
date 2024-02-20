b8609c6579b85b58e638674d0a5c7ac43522d80e
Jacob <hughes.jacobl@gmail.com>
2024-02-20 10:07:58 -0800

    fixes

--- instrument-battle/src/managers/ui.gd
+++ instrument-battle/src/managers/ui.gd
@@ -1,10 +1,7 @@
 extends CanvasLayer
 
-<<<<<<< HEAD
-@onready var arena = %MoneyValue
-=======
 @onready var money_value = %MoneyValue
->>>>>>> e8c9c3347f2def9674f039cbef49f1ca82c0878f
+
 
 
 # Called when the node enters the scene tree for the first time.
