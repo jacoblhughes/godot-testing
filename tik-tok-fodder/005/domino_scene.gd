d91e33b4a6a1a9fef659e0d32a641f8ee019334f
Jacob <hughes.jacobl@gmail.com>
2024-02-23 20:41:41 -0800

    more

--- tik-tok-fodder/005/domino_scene.gd
+++ tik-tok-fodder/005/domino_scene.gd
@@ -1,13 +1,14 @@
-extends RigidBody3D
+extends Node3D
 
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+	var rotation_normalized = rotation.normalized()
+	%Domino.constant_force = gravity * rotation_normalized
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
 	pass
