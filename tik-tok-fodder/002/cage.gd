f218549da87cc18201be123b15d83fc9bcbcb510
Jacob Hughes <hughes.jacobl@gmail.com>
2024-02-28 17:53:56 -0800

    more

---
+++ tik-tok-fodder/002/cage.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+@export var conveyor : Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body is BowlingBall002:
+		conveyor.play_all()
+		pass # Replace with function body.
