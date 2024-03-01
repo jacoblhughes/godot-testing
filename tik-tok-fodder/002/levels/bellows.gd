23a08c8ddb093f6c8e65c1ef5e214293062d908f
Jacob <hughes.jacobl@gmail.com>
2024-02-29 19:13:26 -0800

    more

---
+++ tik-tok-fodder/002/levels/bellows.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body is BowlingBall002:
+		$AnimatedSprite2D.play("default")
+	pass # Replace with function body.
