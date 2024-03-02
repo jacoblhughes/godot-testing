74ffea912c0e2cd040d861bbf1365c9964a1fd33
Jacob <hughes.jacobl@gmail.com>
2024-03-02 15:27:28 -0800

    more

--- tik-tok-fodder/002/intermediates/cage.gd
+++ tik-tok-fodder/002/intermediates/cage.gd
@@ -1,11 +1,11 @@
 #extends StaticBody2D
-extends Starter002
+extends Intermediate002
 
 @export var conveyor : StaticBody2D
 @export var belt : Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("cage: ",self.get_class())
+
 	pass # Replace with function body.
 
 
@@ -17,6 +17,7 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 
 	if body is Starter002:
+
 		%AnimatedSprite2D.play("default")
 		if conveyor:
 			conveyor.run_conveyor()
