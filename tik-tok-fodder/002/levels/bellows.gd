4730fe3a4139c04bf390d1ae9391fb5e0f04af7d
Jacob Hughes <hughes.jacobl@gmail.com>
2024-03-03 10:47:42 -0800

    more

--- tik-tok-fodder/002/levels/bellows.gd
+++ tik-tok-fodder/002/levels/bellows.gd
@@ -14,4 +14,8 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	if body is Starter002:
 		$AnimatedSprite2D.play("default")
+		for overlap in  %Blow.get_overlapping_bodies():
+			if overlap is Intermediate002:
+				overlap.apply_central_impulse(Vector2(1000,100)*scale.x)
+			
 	pass # Replace with function body.
