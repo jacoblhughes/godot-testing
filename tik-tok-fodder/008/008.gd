b69fd295601a93fc3dc2c02a9ef4da860da824e3
Jacob Hughes <hughes.jacobl@gmail.com>
2024-03-04 15:52:16 -0800

    more

---
+++ tik-tok-fodder/008/008.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("move_right"):
+		%Ball.apply_central_impulse(Vector2(250,0))
+	pass
