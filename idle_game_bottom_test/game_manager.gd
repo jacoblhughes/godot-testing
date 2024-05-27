48bbb6351c250e0bedfe83b12227aca4e41dcd1b
Jacob <hughes.jacobl@gmail.com>
2024-05-26 20:27:57 -0700

    continued

--- idle_game_bottom_test/game_manager.gd
+++ idle_game_bottom_test/game_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var window_height = 1080/5
-
+var cell_size = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,3 +16,4 @@ func set_window_height(val):
 
 func get_window_height():
 	return window_height
+
--- idle_game_bottom_test/player.gd
+++ idle_game_bottom_test/player.gd
@@ -1,25 +1,39 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var start_position : Marker2D
 @export var end_position : Marker2D
 
+@export var navigation_agent: NavigationAgent2D
+
 var moving_to_end = true
+var can_move = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
 	get_parent().nodes_moved.connect(_on_nodes_moved)
 	input_event.connect(_on_input_event)
+	NavigationServer2D.map_set_cell_size(NavigationServer2D.get_maps()[0],16.0)
+	NavigationServer2D.map_changed.connect(_on_map_ready)
 
+	
 func _on_nodes_moved():
 	position = start_position.position
 	show()
 	await get_tree().create_timer(2).timeout
-	_move()
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	if can_move:
+		var direction = Vector2.ZERO
+		navigation_agent.target_position = end_position.position
+		direction = navigation_agent.get_next_path_position()-GameManager.get_cell_size()/2 - global_position
+		direction = direction.normalized()
+	
+		velocity = 1000 * direction * delta
+		move_and_slide()
 	pass
 
 func _on_input_event(viewport, event, shape_idx):
@@ -30,15 +44,18 @@ func _on_tween_finished():
 	_move()
 
 func _move():
-	var tween = get_tree().create_tween()
-	tween.finished.connect(_on_tween_finished)
-	if moving_to_end:
-		tween.tween_property(self, "position",end_position.position, 5)
-		moving_to_end = false
-		return
-	else:
-		tween.tween_property(self, "position",start_position.position, 5)
-		moving_to_end = true
-		return
+	#var tween = get_tree().create_tween()
+	#tween.finished.connect(_on_tween_finished)
+	#if moving_to_end:
+		#tween.tween_property(self, "position",end_position.position, 5)
+		#moving_to_end = false
+		#return
+	#else:
+		#tween.tween_property(self, "position",start_position.position, 5)
+		#moving_to_end = true
+		#return
 
 	pass
+	
+func _on_map_ready(map):
+	can_move = true
