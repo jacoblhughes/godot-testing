1d3ac16df3d8574a8f8b00f2ebab2fa3cad36ba1
Jacob <hughes.jacobl@gmail.com>
2024-06-02 08:23:04 -0700

    continued

--- idle_game_bottom_test/collector.gd
+++ idle_game_bottom_test/collector.gd
@@ -4,23 +4,25 @@ extends CharacterBody2D
 @export var tile_map : TileMap
 var dump_location
 var garbage_locations : Array = []
-var garbage_exists = false
+var garbage_seeking = false
 var holding_garbage = false
 var garbage_for_target = null
 var garbage_for_target_location = null
 var speed = 100
 var acceleration = 7
-
+var max_garbage_storage = 2
+var current_garbage_in_storage = 0
+var recycle_location = null
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 func _ready():
-	navigation_agent.target_reached.connect(_on_target_reached)
+	navigation_agent.target_reached.connect(_on_garbage_target_reached)
 
 #
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 	print(garbage_locations)
-	if not garbage_locations.is_empty() and not garbage_exists:
-		garbage_exists = true
+	if not garbage_locations.is_empty() and not garbage_seeking:
+		garbage_seeking= true
 		garbage_for_target = garbage_locations[0]
 		garbage_for_target_location = tile_map.map_to_local(garbage_for_target)
 		navigation_agent.target_position = garbage_for_target_location
@@ -35,8 +37,11 @@ func _physics_process(delta):
 func provide_garbage(garbage_array):
 	garbage_locations = garbage_array
 
-func _on_target_reached():
+func _on_garbage_target_reached():
 	tile_map.erase_cell(3,garbage_for_target)
-	garbage_exists = false
+	garbage_seeking = false
 	garbage_locations.clear()
+	current_garbage_in_storage += 1
 	pass
+
+
